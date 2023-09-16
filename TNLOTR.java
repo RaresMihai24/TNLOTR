703733410d39cb8474a34b690a4966f8f3c337ad
America232 <126671528+america232@users.noreply.github.com>
2023-09-16 15:57:52 +0200

    Add files via upload

---
+++ TNLOTR.java
@@ -0,0 +1,13 @@
+package tnlotr;
+/**
+ *
+ * @author azogprofanatorul
+ */
+public class TNLOTR {
+
+    public static void main(String[] args) {
+        IntroPage start = new IntroPage();
+
+    }
+    
+}
