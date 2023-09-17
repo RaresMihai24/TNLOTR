c166de9e345119f42550020cfdec59057968a5f4
America232 <126671528+america232@users.noreply.github.com>
2023-09-17 19:10:59 +0200

    updated to maven

--- TNLOTR.java
+++ TNLOTR.java
@@ -1,13 +1,18 @@
-package tnlotr;
+package com.mycompany.tnlotr1;
+
+import java.io.IOException;
+import java.sql.SQLException;
+import javax.sound.sampled.LineUnavailableException;
+import javax.sound.sampled.UnsupportedAudioFileException;
+
 /**
  *
- * @author azogprofanatorul
+ * @author Rares
  */
-public class TNLOTR {
+public class TNLOTR1 {
 
-    public static void main(String[] args) {
+    public static void main(String[] args) throws LineUnavailableException, UnsupportedAudioFileException, IOException, SQLException {
         IntroPage start = new IntroPage();
 
     }
-    
 }
