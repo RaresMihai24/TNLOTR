0d4ae4b6092108268ebd1057da773fd49a684370
America232 <126671528+america232@users.noreply.github.com>
2023-09-18 14:56:24 +0200

    fully working register/login

--- IntroPage.java
+++ IntroPage.java
@@ -10,10 +10,15 @@ import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import java.awt.event.FocusEvent;
 import java.awt.event.FocusListener;
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
 import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.FileWriter;
 import java.io.IOException;
+import java.io.PrintWriter;
 import java.net.InetAddress;
-import java.net.PasswordAuthentication;
 import java.net.UnknownHostException;
 import java.sql.Connection;
 import java.sql.DriverManager;
@@ -22,11 +27,11 @@ import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.util.Properties;
 import java.util.Random;
+import java.util.Scanner;
 import java.util.logging.Level;
 import java.util.logging.Logger;
 import javax.mail.Message;
 import javax.mail.MessagingException;
-import javax.mail.Session;
 import javax.mail.Transport;
 import javax.mail.internet.InternetAddress;
 import javax.mail.internet.MimeMessage;
@@ -47,7 +52,7 @@ public class IntroPage extends JFrame{
     public final int width = 1920;
     public final int height = 1080;
     JCheckBox checkBox = new JCheckBox("I agree to the terms and conditions");
-    JCheckBox checkBox2 = new JCheckBox("I want to be remembered");
+    JCheckBox checkBox2 = new JCheckBox("I want to be remembered", true);
     JPanel contentPane = new JPanel() {
         @Override
         protected void paintComponent(Graphics g) {
@@ -59,8 +64,16 @@ public class IntroPage extends JFrame{
     JPanel loginPanel = new JPanel();
     JPanel registerPanel = new JPanel();
     JPanel selectLanguage = new JPanel();
-
+    JTextField usernameLogin = createPlaceholderTextField("Username");
+    JPasswordField passwordLogin = createPlaceholderPasswordField("Password");
+    JTextField usernameRegister = createPlaceholderTextField("Username");
+    JPasswordField registerPassword = createPlaceholderPasswordField("123456789");
+    JPasswordField registerPasswordConf = createPlaceholderPasswordField("123456789");
+    JTextField emailRegister = createPlaceholderTextField("example@address.com");
+    JTextField refferalID = createPlaceholderTextField("Referral ID");
+        
     public IntroPage() throws LineUnavailableException, UnsupportedAudioFileException, IOException, SQLException {
+        loadCredentialsFromFile();
         File soundFile = new File("C:\\Users\\Rares\\Documents\\NetBeansProjects\\TNLOTR1\\IntroSong.wav");
         AudioInputStream audioIn = AudioSystem.getAudioInputStream(soundFile);
         Clip clip = AudioSystem.getClip();
@@ -69,20 +82,12 @@ public class IntroPage extends JFrame{
         
         frame = new JFrame("TNLOTR");
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-
-        JTextField usernameLogin = createPlaceholderTextField("Username");
-        usernameLogin.setPreferredSize(new Dimension(160, 30));
-        JPasswordField passwordLogin = createPlaceholderPasswordField("Password");
         passwordLogin.setPreferredSize(new Dimension(160, 30));
-        JTextField usernameRegister = createPlaceholderTextField("Username");
+        usernameLogin.setPreferredSize(new Dimension(160, 30));
         usernameRegister.setPreferredSize(new Dimension(160, 30));
-        JPasswordField registerPassword = createPlaceholderPasswordField("123456789");
         registerPassword.setPreferredSize(new Dimension(160, 30));
-        JPasswordField registerPasswordConf = createPlaceholderPasswordField("123456789");
         registerPasswordConf.setPreferredSize(new Dimension(160, 30));
-        JTextField emailRegister = createPlaceholderTextField("example@address.com");
         emailRegister.setPreferredSize(new Dimension(160, 30));
-        JTextField refferalID = createPlaceholderTextField("Referral ID");
         refferalID.setPreferredSize(new Dimension(160, 30));
 
         backgroundImage = new ImageIcon("C:\\Users\\Rares\\Documents\\NetBeansProjects\\TNLOTR1\\background.jpg").getImage();
@@ -112,8 +117,76 @@ public class IntroPage extends JFrame{
                     Logger.getLogger(IntroPage.class.getName()).log(Level.SEVERE, null, ex);
                 }
             }
-        });
+        }
+);
         
+buttonLogin.addActionListener(new ActionListener() {
+    @Override
+    public void actionPerformed(ActionEvent e) {
+        String username = usernameLogin.getText();
+        String password = String.valueOf(passwordLogin.getPassword());
+
+        if (username.isEmpty() || password.isEmpty()) {
+            JOptionPane.showMessageDialog(null, "Please enter both username and password.");
+            return;
+        }
+
+        try {
+            if (verifyLogin(username, password)) {
+                if (checkBox2.isSelected()) {
+                    saveCredentialsToFile(username, password);
+                }
+                
+                JOptionPane.showMessageDialog(null, "Login performed succesfully. Entering the game in a second.");
+                contentPane.remove(loginPanel);
+                contentPane.remove(registerPanel);
+                
+                frame.revalidate();
+                frame.repaint();
+            } else {
+                JOptionPane.showMessageDialog(null, "Invalid username or password.");
+            }
+        } catch (SQLException ex) {
+            Logger.getLogger(IntroPage.class.getName()).log(Level.SEVERE, null, ex);
+        } catch (IOException ex) {
+            Logger.getLogger(IntroPage.class.getName()).log(Level.SEVERE, null, ex);
+        }
+    }
+});
+checkBox2.addActionListener(new ActionListener() {
+    @Override
+    public void actionPerformed(ActionEvent e) {
+        if (checkBox2.isSelected()) {
+            try (PrintWriter writer = new PrintWriter("saved_account")) {
+                writer.println(usernameLogin.getText());
+                writer.println(passwordLogin.getText());
+            } catch (FileNotFoundException ex) {
+                ex.printStackTrace();
+            }
+        } else {
+            File savedAccountFile = new File("saved_account");
+            if (savedAccountFile.exists()) {
+                savedAccountFile.delete();
+                usernameLogin.setText("");
+                passwordLogin.setText("");
+            }
+        }
+    }
+});
+
+File savedAccountFile = new File("saved_account");
+if (savedAccountFile.exists() && checkBox2.isSelected()) {
+    try (Scanner scanner = new Scanner(savedAccountFile)) {
+        String savedUsername = scanner.nextLine();
+        String savedPassword = scanner.nextLine();
+        usernameLogin.setText(savedUsername);
+        passwordLogin.setText(savedPassword);
+    } catch (FileNotFoundException ex) {
+        ex.printStackTrace();
+    }
+}
+
+
 
         GridBagConstraints gbc = new GridBagConstraints();
         gbc.insets = new Insets(4, 4, 5, 5);
@@ -333,13 +406,13 @@ private void sendEmail(String recipientEmail, String verificationCode) {
     javax.mail.Session mailSession = javax.mail.Session.getInstance(props, new javax.mail.Authenticator() {
         @Override
         protected javax.mail.PasswordAuthentication getPasswordAuthentication() {
-            return new javax.mail.PasswordAuthentication("zzzzzzzzzzzzzzzz", "yyyyyyyyyyyyyyyy");
+            return new javax.mail.PasswordAuthentication("zzzzzzzzzzzm", "zzzzzzzzzzw");
         }
     });
 
     try {
         Message message = new MimeMessage(mailSession);
-        message.setFrom(new InternetAddress("zzzzzzzzzzzz.com")); 
+        message.setFrom(new InternetAddress("zzzzzzzzl.com")); 
         message.setRecipients(Message.RecipientType.TO, InternetAddress.parse(recipientEmail));
         message.setSubject("Account Verification");
         message.setText("""
@@ -355,10 +428,56 @@ private void sendEmail(String recipientEmail, String verificationCode) {
         System.out.println("Email sent successfully");
 
     } catch (MessagingException e) {
+    }
+}
+
+private boolean verifyLogin(String username, String password) throws SQLException {
+    Connection con = null;
+    PreparedStatement preparedStatement = null;
+    ResultSet resultSet = null;
+
+    try {
+        con = DriverManager.getConnection("jdbc:mysql://localhost/alpha", "root", "");
+
+        String query = "SELECT COUNT(*) FROM accounts WHERE account_name = ? AND password = ?";
+        preparedStatement = con.prepareStatement(query);
+        preparedStatement.setString(1, username);
+        preparedStatement.setString(2, password);
+
+        resultSet = preparedStatement.executeQuery();
+
+        if (resultSet.next() && resultSet.getInt(1) > 0) {
+            return true; 
+        } else {
+            return false; 
+        }
+    } finally {
+        if (resultSet != null) resultSet.close();
+        if (preparedStatement != null) preparedStatement.close();
+        if (con != null) con.close();
+    }
+}
+
+private void saveCredentialsToFile(String username, String password) throws IOException {
+    BufferedWriter writer = new BufferedWriter(new FileWriter("saved_account.txt"));
+    writer.write(username + "," + password);
+    writer.close();
+}
+
+private void loadCredentialsFromFile() {
+    try {
+        File file = new File("saved_account.txt");
+        if (file.exists()) {
+            BufferedReader reader = new BufferedReader(new FileReader(file));
+            String[] credentials = reader.readLine().split(",");
+            usernameLogin.setText(credentials[0]);
+            passwordLogin.setText(credentials[1]);
+        }
+    } catch (IOException e) {
         e.printStackTrace();
     }
 }
-    
+
 }
     
 
